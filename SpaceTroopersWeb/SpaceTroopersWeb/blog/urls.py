280ae350ff7de15eaf2e11c8e0b349e260dbbf40
Yusufpek <yusuf55ipek55@gmail.com>
2023-02-26 17:25:58 +0300

    dosya düzenleme

--- SpaceTroopersWeb/SpaceTroopersWeb/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('post', views.PostList.as_view(), name='post'),
-    path('post-index', views.PostListIndex.as_view(), name='post_index'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-]
--- SpaceTroopersWeb/blog/urls.py
+++ SpaceTroopersWeb/blog/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.home, name='home'),
+    path('post', views.PostList.as_view(), name='post'),
+    path('post-index', views.PostListIndex.as_view(), name='post_index'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
